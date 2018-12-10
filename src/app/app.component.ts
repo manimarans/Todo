f1cfc7b22295ef1f000193bb10ae2497efcd6717
Manimaran S <manimaran@navitaslifesciences.com>
2018-12-10 19:15:35 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Task } from './Task';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app-todo';
+  name:string;
+  tasks:Task[];
+  constructor(){
+    this.tasks = [];
+  }
+ addTask(data):void{
+
+    console.log("Clicked"+data);
+    let task:Task={
+      id : '',
+      name:''
+    };
+    if(data.key == 'Enter'){
+      task.name = this.name;
+      console.log(task.name);
+      this.tasks.push(task);
+      this.name='';
+      console.log("Clicked End");
+    }
+  }
+
+}
